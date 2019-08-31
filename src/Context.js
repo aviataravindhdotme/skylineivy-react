854b36add6b52e924977a3c69b73f19347fdb94d
Avi <avi@aravindh.me>
2019-08-30 23:48:13 -0400

    context bug fixing

--- src/Context.js
+++ src/Context.js
@@ -1,28 +1,40 @@
 import React, {useReducer} from 'react';
-function useReducer (state, action) {
-    switch(action.type){
-        case 'add': return {...state, count:state.count+1};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "Add":{
+            const newState = Object.assign({},
+                state,
+                {cartTotal:state.cartTotal+1},
+                {cartItems: state.cartItems.push({id:payload.id, state.cartTotal+1})};
 
-        case "remove":
-            return {...state, count:state.count-1};
+            return newState;
+        }
 
-
+        case "Remove":{
+            const newState = Object.assign({},
+                state, {cartTotal:state.cartTotal>0? state.cartTotal-1: 0});
+            return newState;
+        }
         default:
             return;
     }
+};
 
-}
-const cartState = {count:5};
+const initialCart = {cartTotal: 5, cartItems:[{
+    id:"fa86b1c-9ebf-4555-987f-7b8bf7d27be7",
+        count:5
+    }]};
 
-const CartContext= React.createContext(cartState);
+const CartContext= React.createContext(initialCart);
 
 function CartProvider(props){
+    const [state, dispatch] = useReducer(reducer, initialCart);
 
     return(
-      <CartContext.Provider value={cartState}>
+      <CartContext.Provider value={{state, dispatch}}>
           {props.children}
       </CartContext.Provider>
     );
 }
 
-export {CartContext, CartProvider}
+export {CartContext, CartProvider};
